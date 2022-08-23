b7686928ef856a32b787f1d4649d5e6f23448081
Roman <AverkievRE@yandex.ru>
2022-08-23 15:54:33 +0300

    Добавлен метод TelegramBotMetod

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -28,118 +28,15 @@ using System;
 using System.Net;
 using System.Text;
 using System.Linq;
-using Telegram.Bot;
+ using Telegram.Bot;
 using System.Net.Http;
 
 namespace TelegramBot
 {
     class Program
     {
-        static TelegramBotClient bot;
         static void Main(string[] args)
         {
-            string token = File.ReadAllText(@"C:\Test\token.txt");
-            string tokenYandex = File.ReadAllText(@"C:\Test\yandex_ru.txt");
-
-            Console.WriteLine(token);
-
-            WebClient webClient = new WebClient() { Encoding = Encoding.UTF8 };
-
-            int update_id = 0;
-            string startUrl = $@"https://api.telegram.org/bot{token}/";
-            string yandexStartUrl = 
-                @"https://dictionary.yandex.net/api/v1/dicservice.json/";
-
-            Console.WriteLine(startUrl);
-
-            while (true)
-            {
-                string url = $"{startUrl}getUpdates?offset={update_id}";
-                var r = webClient.DownloadString(url);
-
-                var msgs = JObject.Parse(r)["result"].ToArray();
-
-                
-
-                foreach (dynamic msg in msgs)
-                {
-                    Console.WriteLine(msg);
-                    update_id = Convert.ToInt32(msg.update_id) + 1;
-
-                    string userMessage = msg.message.text;
-                    string userId = msg.message.chat.id;
-                    string userFirstName = msg.message.from.first_name;
-
-                    string text = $"{userFirstName} {userId} {userMessage}";
-                    string responseText;
-
-                    if (msg.message.document != null)
-                    {
-                        Console.WriteLine(text);
-
-                        responseText = "Не шли мне текстовые файлы";
-                        url = $"{startUrl}sendMessage?chat_id={userId}&text={responseText}";
-                        webClient.DownloadString(url);
-
-                    }
-                    else if (msg.message.forward_from_chat != null)
-                    {
-                        responseText = "Зачем ты мне это переслал";
-                        url = $"{startUrl}sendMessage?chat_id={userId}&text={responseText}";
-                        webClient.DownloadString(url);
-                    }
-                    else if (userMessage == "/Привет")
-                    {
-                        responseText = $"Привет {userFirstName}, Что - то хотели";
-                        url = $"{startUrl}sendMessage?chat_id={userId}&text={responseText}";
-                        webClient.DownloadString(url);
-                        while (true)
-                        {
-                            url = $"{startUrl}getUpdates?offset={update_id}";
-                            r =webClient.DownloadString(url);
-                            Console.WriteLine(r);
-                            Console.ReadLine();
-
-                            msgs = JObject.Parse(r)["result"].ToArray();
-
-                            foreach (dynamic ms in msgs)
-                            {
-                                userMessage = ms.message.text;
-                                userId = ms.message.chat.id;
-                                userFirstName = ms.message.from.first_name;
-
-                                if(userMessage == "/Перевод")
-                                {
-                                    url = $"{yandexStartUrl}getLangs?key={tokenYandex}";
-                                    r = webClient.DownloadString(url);
-
-                                    responseText = $"{r}";
-                                    url = $"{startUrl}sendMessage?chat_id={userId}&text={responseText}";
-                                    webClient.DownloadString(url);
-
-                                    msgs = JObject.Parse(r)[","].ToArray() ;
-
-                                    Console.WriteLine(msgs);
-
-                                    Console.ReadLine();
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        //responseText = userMessage;
-                        //Console.WriteLine(responseText);
-                        //url = $"{startUrl}sendMessage?chat_id={userId}&text={responseText}";
-                        //webClient.DownloadString(url);
-                    }
-                }
-
-
-                Thread.Sleep(100);
-            }
-
         }
-
     }
 }
--- TelegramBot/TelegramBot.cs
+++ TelegramBot/TelegramBot.cs
@@ -3,10 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Telegram.Bot;
+using System.Text;
 
 namespace TelegramBot
 {
     internal class TelegramBot
     {
+        #region Поля
+        private string _token;         // Ключ доступа
+        private string _startUrl;      // Стартовый URL    
+        private int _update_id;
+        
+
+        #endregion
+
+        #region Конструкторы
+
+        /// <summary>
+        /// Конструктор по умолчянию
+        /// </summary>
+        public TelegramBot()
+        {
+            this._token = File.ReadAllText(@"C:\Test\token.txt");
+            this._startUrl = $@"https://api.telegram.org/bot{_token}/";
+            this._update_id = 0;
+        }
+        #endregion
+
+        #region Методы
+
+        /// <summary>
+        /// Чтение данных согласно методу
+        /// </summary>
+        /// <param name="webClient">Web поток</param>
+        /// <param name="metod">Название метода</param>
+        /// <returns></returns>
+        public  string TelegramBotMetod(WebClient webClient, string metod)
+        {
+            string url = $"{this._startUrl}{metod}?offset={this._update_id}";
+            string r = webClient.DownloadString(url);
+            return r;
+        }
+        #endregion
     }
 }
