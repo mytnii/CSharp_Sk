a69979b68941850f1935f2cd824c71a8eedb157a
Roman <AverkievRE@yandex.ru>
2022-10-09 17:51:55 +0300

    Добавлен метод ConsoleOutput

---
+++ TelegramBot/ConsoleOperation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    internal class ConsoleOperation
+    {
+        #region Методы
+
+        /// <summary>
+        /// Вывод в консоль
+        /// </summary>
+        /// <param name="str">Строка</param>
+        public static void ConsoleOutput(ref string str)
+        {
+            Console.WriteLine(str);
+        }
+        #endregion
+    }
+}
