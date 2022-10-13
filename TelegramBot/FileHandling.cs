a415962cce6df09c176f82046f40e64faf94c9cc
Roman <AverkievRE@yandex.ru>
2022-10-13 19:59:24 +0300

    Добавлен метод  FillingAsync

---
+++ TelegramBot/FileHandling.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    internal class FileHandling
+    {
+        /// <summary>
+        /// Запись в файл
+        /// </summary>
+        /// <param name="fileName">Название скачаного файла</param>
+        /// <returns></returns>
+        #region Методы
+        public static async Task FillingAsync(string fileName)
+        {
+            FileStream fileStram = new FileStream("uploadedFiles.txt", FileMode.Append);
+            await fileStram.WriteAsync(Encoding.UTF8.GetBytes(fileName));
+
+            fileStram.Close();
+        }
+     
+        #endregion
+    }
+}
