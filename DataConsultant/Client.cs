2ec2adb4ef43612c09b63238b1d5e5ec1d84974f
Roman <AverkievRE@yandex.ru>
2022-12-24 16:51:18 +0300

    Added fields

---
+++ DataConsultant/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataConsultant
+{
+    internal class Client
+    {
+        #region Поля
+        private string _lastName;            // Фамилия
+        private string _firstName;           // Имя
+        private string _patronumic;          // Отчество
+        private int _series;                 // Серия паспорта
+        private int _numberPassporty;        // Номер паспорта
+        public List<string> phoneNumber;     // Номера телефонов
+
+        #endregion
+    }
+}
